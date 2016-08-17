a232b04a2822d1d90fd4bcaba37986ab9609be5b
Jon Linklater-Johnson <jon@binarytales.com>
2016-08-18 00:47:58 +0100

    Improved acceptance test by testing returned values agains the actual values in the mock data instead of a hardcoded value

--- tests/acceptance/show-detail-test.js
+++ tests/acceptance/show-detail-test.js
@@ -5,31 +5,33 @@ moduleForAcceptance('Acceptance | show detail');
 
 test('visiting topic detail', function(assert) {
 
-  //server.loadFixtures('topics');
-
   visit('/topics/1751295897__Berlin');
+
+  var berlinData = server.db.topics.find('1751295897__Berlin');
 
   andThen(function() {
     assert.equal(currentURL(), '/topics/1751295897__Berlin');
 
-    assert.equal(find('.detail-value-volume').text(), 165);
+    assert.equal(find('.detail-value-volume').text(), berlinData.volume);
 
-    assert.equal(find('.detail-value-positive').text(), 29);
-    assert.equal(find('.detail-value-negative').text(), 3);
-    assert.equal(find('.detail-value-neutral').text(), 133);
+    assert.equal(find('.detail-value-positive').text(), berlinData.sentiment.positive);
+    assert.equal(find('.detail-value-negative').text(), berlinData.sentiment.negative);
+    assert.equal(find('.detail-value-neutral').text(), berlinData.sentiment.neutral);
   });
 
 
   click('#Text1751295897Hammered');
 
+  var hammeredData = server.db.topics.find('1751295897__Hammered');
+
   andThen(function() {
     assert.equal(currentURL(), '/topics/1751295897__Hammered');
 
-    assert.equal(find('.detail-value-volume').text(), 48);
+    assert.equal(find('.detail-value-volume').text(), hammeredData.volume);
 
     assert.equal(find('.detail-value-positive').length, 0, "Positive view should have been removed");
-    assert.equal(find('.detail-value-negative').text(), 30);
-    assert.equal(find('.detail-value-neutral').text(), 18);
+    assert.equal(find('.detail-value-negative').text(), hammeredData.sentiment.negative);
+    assert.equal(find('.detail-value-neutral').text(), hammeredData.sentiment.neutral);
   });
 
 });
